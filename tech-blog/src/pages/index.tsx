710913ff6dee4372470fa38f15fe2c21ee437752
Kaywin A. <kay.afanasiff@gmail.com>
2025-09-03 17:27:07 +0000

    Adding hero image to index page; tweaking layout for best fit

--- tech-blog/src/pages/index.tsx
+++ tech-blog/src/pages/index.tsx
@@ -9,23 +9,12 @@ export default function Home(): JSX.Element {
         <title>Home</title>
         <meta name="description" content="The Docs Slayer Landing Page" />
       </head>
-      <main style={{ padding: '4rem 1rem', maxWidth: 800, margin: '0 auto' }}>
+      <main style={{ padding: '1.5rem', maxWidth: 800, margin: '0 auto' }}>
         <h1>⚔️ The Docs Layer ⚔️</h1>
+        <img src="img/hero.png" alt="The Docs Layer logo" width="256"></img>
         <p>Technical documentation clean and simple.</p>
-
-        <ul>
-          <li>
-            <Link to="/blog/welcome">Welcome</Link>
-          </li>
-          <li>
-            <Link to="/blog/costs-of-automation">Costs of Automation</Link>
-          </li>
-          <li>
-            <Link to="/blog/automation-action-plan">Automation Action Plan</Link>
-          </li>
-        </ul>
-
-        <p style={{ marginTop: '2rem' }}>
+        <Link to="/blog/welcome">Welcome</Link>
+        <p style={{ marginTop: "1.5rem" }}>
           <Link className="button button--primary" to="/blog">
             View All Posts
           </Link>
